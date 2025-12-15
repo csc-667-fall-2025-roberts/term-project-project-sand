d0706eec30115f07340e5bf9f9380cf6f345543f
Daniel Mulvad <daniel@dmulvad.com>
2025-12-14 21:32:38 -0800

    cleanup root dir

--- src/backend/routes/dashboard.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-const router = express.Router();
-
-router.get("/", (_req, res) => {
-  res.redirect("/lobby");
-});
-
-export { router as dashboardRouter }; // to be used in server.ts
--- src/backend/routes/login.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-
-const router = express.Router();
-
-router.get("/", (_req, res) => {
-  res.redirect("/auth/login");
-});
-
-export { router as loginRouter }; // to be used in server.ts
